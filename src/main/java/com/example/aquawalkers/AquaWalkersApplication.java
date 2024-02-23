d8622df409c734ca8c661725a1a0a3f030b53cc0
oalba2022 <o.alba.2022@alumnos.urjc.es>
2024-02-23 12:10:25 +0100

    Inicio

---
+++ src/main/java/com/example/aquawalkers/AquaWalkersApplication.java
@@ -0,0 +1,13 @@
+package com.example.aquawalkers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaWalkersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquaWalkersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aquawalkers/AquaWalkersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aquawalkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaWalkersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
