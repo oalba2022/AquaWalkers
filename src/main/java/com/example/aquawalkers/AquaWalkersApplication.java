7505f2e67c0cb50916995aa79eb640a2f1ba196b
oalba2022 <o.alba.2022@alumnos.urjc.es>
2024-02-26 23:02:19 +0100

    Muchas cosas

--- src/main/java/com/example/aquawalkers/AquaWalkersApplication.java
+++ src/main/java/com/example/aquawalkers/AquaWalkersApplication.java
@@ -2,10 +2,11 @@ package com.example.aquawalkers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class AquaWalkersApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AquaWalkersApplication.class, args);
     }
--- src/main/java/com/example/aquawalkers/controladores/EnlacesController.java
+++ src/main/java/com/example/aquawalkers/controladores/EnlacesController.java
@@ -1,9 +1,11 @@
 package com.example.aquawalkers.controladores;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+@Controller
 public class EnlacesController {
 
     @GetMapping("/inicio")
