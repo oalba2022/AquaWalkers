fe41fcbd83817dd6ddcdba0047df5468699a6190
oalba2022 <o.alba.2022@alumnos.urjc.es>
2024-02-26 20:25:09 +0100

    Cambio nombres de EnlacesController

--- src/main/java/com/example/aquawalkers/controladores/EnlacesController.java
+++ src/main/java/com/example/aquawalkers/controladores/EnlacesController.java
@@ -3,10 +3,18 @@ package com.example.aquawalkers.controladores;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-public class EnalcesController {
+
+public class EnlacesController {
 
     @GetMapping("/inicio")
     public String inicio(Model model){
         return "index";
     }
+
+
+    @GetMapping("/zapatilla")
+    public String zapatilla(Model model){
+        return "zapatilla";
+    }
 }
+
