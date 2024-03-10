d012228974c80f7c4a84553bf5f891cb302a1fd7
Al3xM4rki <alexmarqui11@gmail.com>
2024-03-10 10:21:37 +0100

    ShoeNotFound exception handler.

---
+++ src/main/java/com/example/aquawalkers/exceptions/dto/ExceptionMessage.java
@@ -0,0 +1,14 @@
+package com.example.aquawalkers.exceptions.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor //añade todos los setters y getters & constructor
+@NoArgsConstructor  //añade constructor vacio
+public class ExceptionMessage {
+    private HttpStatus status;
+    private String message;
+}
