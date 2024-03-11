75ac52bcf62dc01d77e2b9df9ceb50c89210c3ca
oalba2022 <o.alba.2022@alumnos.urjc.es>
2024-03-11 23:36:30 +0100

    Shoes Greetings

--- src/main/java/com/example/aquawalkers/controllers/LinksController.java
+++ src/main/java/com/example/aquawalkers/controllers/LinksController.java
@@ -1,16 +1,33 @@
 package com.example.aquawalkers.controllers;
 
+import com.example.aquawalkers.exceptions.ShoeNotFoundException;
+import com.example.aquawalkers.models.Shoe;
+import com.example.aquawalkers.service.ShoeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 
 @Controller
 public class LinksController {
 
+    @Autowired
+    private ShoeService shoeService;
+
     @GetMapping("/inicio")
 
-    public String inicio(Model model){
+    public String inicio(Model model) throws ShoeNotFoundException {
+        if(this.shoeService.findAll().size() >= 3){
+            Shoe zapa1 = this.shoeService.findById(1L).get();
+            model.addAttribute("zapa1",zapa1);
+            Shoe zapa2 = this.shoeService.findById(2L).get();
+            model.addAttribute("zapa2",zapa2);
+            Shoe zapa3 = this.shoeService.findById(3L).get();
+            model.addAttribute("zapa3",zapa3);
+        }
         return "greeting-page";
     }
 
@@ -21,8 +38,16 @@ public class LinksController {
     }
 
     @GetMapping("/")
-    public String register(Model model){
-        return "greeting-page";
+    public String greeting(Model model) throws ShoeNotFoundException {
+        if(this.shoeService.findAll().size() >= 3){
+            Shoe zapa1 = this.shoeService.findById(1L).get();
+            model.addAttribute("zapa1",zapa1);
+            Shoe zapa2 = this.shoeService.findById(2L).get();
+            model.addAttribute("zapa2",zapa2);
+            Shoe zapa3 = this.shoeService.findById(3L).get();
+            model.addAttribute("zapa3",zapa3);
+        }
+            return "greeting-page";
     }
 
     @GetMapping("/sobre-nosotros")
