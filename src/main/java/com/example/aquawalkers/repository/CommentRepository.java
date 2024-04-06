75b39976b3374732b75dad5e09f9245936b82d3f
oalba2022 <o.alba.2022@alumnos.urjc.es>
2024-04-06 12:46:45 +0200

    Creating repositories to connect with BBDD

--- src/main/java/com/example/aquawalkers/repository/CommentRepository.java
+++ src/main/java/com/example/aquawalkers/repository/CommentRepository.java
@@ -1,7 +1,8 @@
 package com.example.aquawalkers.repository;
 
+import com.example.aquawalkers.models.Comment;
 import com.example.aquawalkers.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
---
+++ src/main/java/com/example/aquawalkers/service/DatabaseInitializer.java
@@ -0,0 +1,5 @@
+package com.example.aquawalkers.service;
+
+public class DatabaseInitializer {
+    //Esto es para ya cargar datos una vez hagamos el proyecto. No creo que lo necesitemos ahora
+}
