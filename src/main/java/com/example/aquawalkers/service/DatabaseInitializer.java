d5bc52d69b614d42d8e2e4cf0b968fccce47fc8c
oalba2022 <o.alba.2022@alumnos.urjc.es>
2024-04-16 13:42:38 +0200

    Img on database at start Fix sqli on filters

--- src/main/java/com/example/aquawalkers/service/DatabaseInitializer.java
+++ src/main/java/com/example/aquawalkers/service/DatabaseInitializer.java
@@ -6,13 +6,22 @@ import com.example.aquawalkers.models.Comment;
 import com.example.aquawalkers.repository.ShoeRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.sql.SQLException;
+
+import org.apache.commons.io.IOUtils;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 
 @Component
 public class DatabaseInitializer {
@@ -25,19 +34,27 @@ public class DatabaseInitializer {
     @PostConstruct
     public void init() throws IOException, SQLException {
 
-        // Create some shops
+        File img1 = new File("C:\\Users\\Oscar\\IdeaProjects\\AquaWalkers\\images\\tn.png");
+        FileInputStream input1 = new FileInputStream(img1);
+        MultipartFile multipartFile1 = new MockMultipartFile("fileItem", img1.getName(), "image/png", IOUtils.toByteArray(input1));
+
+        File img2 = new File("C:\\Users\\Oscar\\IdeaProjects\\AquaWalkers\\images\\vans.png");
+        FileInputStream input2 = new FileInputStream(img2);
+        MultipartFile multipartFile2 = new MockMultipartFile("fileItem", img2.getName(), "image/png", IOUtils.toByteArray(input2));
+
+        File img3 = new File("C:\\Users\\Oscar\\IdeaProjects\\AquaWalkers\\images\\crocs3.png");
+        FileInputStream input3 = new FileInputStream(img3);
+        MultipartFile multipartFile3 = new MockMultipartFile("fileItem", img3.getName(), "image/png", IOUtils.toByteArray(input3));
+
         User user1= new User();
         userService.save(user1);
 
-
-
-        // Create some books
         Shoe shoe1 = new Shoe("TN","Nike","Muy baratas",50,28,120,null);
-        shoeService.save1(shoe1);
+        shoeService.save(shoe1,multipartFile1);
         Shoe shoe2 = new Shoe("Adidas 300","Adidas","Muy bonitas y chulas",50,30,50,null);
-        shoeService.save1(shoe2);
+        shoeService.save(shoe2,multipartFile2);
         Shoe shoe3 = new Shoe("Crocks fran de la jungla","Crocs","Tela de javad entro",50,40,1,null);
-        shoeService.save1(shoe3);
+        shoeService.save(shoe3,multipartFile3);
 
         Comment comment1 = new Comment();
         comment1.setText("Hola, cuanto valen");
